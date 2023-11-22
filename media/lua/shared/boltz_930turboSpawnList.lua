10bd4656f71245bbd1c499cbc595778d437d8275
andydbui <andy1000ek@hotmail.com>
2023-11-22 16:34:42 -0500

    setting up initial files

---
+++ media/lua/shared/boltz_930turboSpawnList.lua
@@ -0,0 +1,10 @@
+if VehicleZoneDistribution then
+
+VehicleZoneDistribution.parkingstall.vehicles["Base.boltz_930turbo"] = {index = -1, spawnChance = 7};
+VehicleZoneDistribution.trailerpark.vehicles["Base.boltz_930turbo"] = {index = -1, spawnChance = 10};
+VehicleZoneDistribution.bad.vehicles["Base.boltz_930turbo"] = {index = -1, spawnChance = 10};
+VehicleZoneDistribution.medium.vehicles["Base.boltz_930turbo"] = {index = -1, spawnChance = 5};
+VehicleZoneDistribution.good.vehicles["Base.boltz_930turbo"] = {index = -1, spawnChance = 2};
+VehicleZoneDistribution.junkyard.vehicles["Base.boltz_930turbo"] = {index = -1, spawnChance = 20};
+
+end
